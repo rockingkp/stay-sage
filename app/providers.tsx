56d374f0241660a75521de9f37ae8e40401106ce
rockingkp <kaamyakpant@gmail.com>
2024-07-28 18:20:01 +0530

    implement user sign out

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,10 +2,12 @@
 
 import { ReactNode } from "react";
 import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
