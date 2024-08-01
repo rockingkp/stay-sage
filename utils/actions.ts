c49d0dfa0503990e54100f3242c5d5f450543639
rockingkp <kaamyakpant@gmail.com>
2024-08-01 16:35:20 +0530

    implement supabase image upload

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { profileSchema, validateWithZodSchema } from "./schemas";
+import { imageSchema, profileSchema, validateWithZodSchema } from "./schemas";
 import db from "./db";
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
@@ -105,5 +105,7 @@ export const updateProfileImageAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
+  const image = formData.get("image") as File;
+  const validatedFields = validateWithZodSchema(imageSchema, { image });
   return { message: "Profile image updated successfully" };
 };
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -26,3 +26,22 @@ export const validateWithZodSchema = <T>(
 
   return result.data;
 };
+
+export const imageSchema = z.object({
+  image: validateFile(),
+});
+
+function validateFile() {
+  const maxUploadSize = 2 * 1024 * 1024;
+  const acceptedFileTypes = ["image/"];
+  return z
+    .instanceof(File)
+    .refine((file) => {
+      return !file || file.size <= maxUploadSize;
+    }, `File size must be less than 1 MB`)
+    .refine((file) => {
+      return (
+        !file || acceptedFileTypes.some((type) => file.type.startsWith(type))
+      );
+    }, "File must be an image");
+}
