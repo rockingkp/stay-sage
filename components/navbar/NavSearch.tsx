a6d3e7f378f8fa60867801f6a4209358358498ed
rockingkp <kaamyakpant@gmail.com>
2024-08-03 17:35:25 +0530

    fix navSearch component on favourites Page search

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { Input } from "../ui/input";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 import { useState, useEffect } from "react";
 
 const NavSearch = () => {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
+
   const { replace } = useRouter();
 
   const [search, setSearch] = useState(
@@ -22,7 +22,7 @@ const NavSearch = () => {
       params.delete("search");
     }
 
-    replace(`${pathname}?${params.toString()}`);
+    replace(`/?${params.toString()}`);
   }, 500);
 
   useEffect(() => {
