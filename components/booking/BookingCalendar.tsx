19424f6e9e0050778005d941d419a4a2fa7eb339
rockingkp <kaamyakpant@gmail.com>
2024-08-05 03:11:00 +0530

    implement block unavailable dates from booking properties

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -16,8 +16,30 @@ import {
 const BookingCalendar = () => {
   const currentDate = new Date();
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+  const bookings = useProperty((state) => state.bookings);
+
+  const { toast } = useToast();
+
+  const blockedPeriods = generateBlockedPeriods({
+    bookings,
+    today: currentDate,
+  });
+
+  const unavailableDates = generateDisabledDates(blockedPeriods);
 
   useEffect(() => {
+    const selectedRange = generateDateRange(range);
+    const isDisabledDateIncluded = selectedRange.some((date) => {
+      if (unavailableDates[date]) {
+        setRange(defaultSelected);
+        toast({
+          description: "Some dates are booked. Please select again",
+        });
+        return true;
+      }
+      return false;
+    });
+
     useProperty.setState({ range });
   }, [range]);
 
@@ -28,6 +50,7 @@ const BookingCalendar = () => {
       selected={range}
       onSelect={setRange}
       className="mb-4"
+      disabled={blockedPeriods}
     />
   );
 };
