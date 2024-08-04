f22704f5c26424c51b6c4f1653ded2feadd26ab3
rockingkp <kaamyakpant@gmail.com>
2024-08-05 02:14:48 +0530

    implement booking form

--- components/booking/BookingContainer.tsx
+++ components/booking/BookingContainer.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import { useProperty } from "@/utils/store";
+import BookingForm from "./BookingForm";
+import ConfirmBooking from "./ConfirmBooking";
+
 const BookingContainer = () => {
-  return <div>BookingContainer</div>;
+  const { range } = useProperty((state) => state);
+
+  if (!range || !range.from || !range.to) return null;
+
+  if (range.to.getTime() === range.from.getTime()) return null;
+
+  return (
+    <div className="w-full">
+      <BookingForm />
+      <ConfirmBooking />
+    </div>
+  );
 };
 
 export default BookingContainer;
