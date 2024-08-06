f5df9fb56127cc9bc17d3762edf0925517e36d61
rockingkp <kaamyakpant@gmail.com>
2024-08-06 15:12:41 +0530

    implement charts container admin page

--- components/admin/ChartsContainer.tsx
+++ components/admin/ChartsContainer.tsx
@@ -1,4 +1,12 @@
-const ChartsContainer = () => {
+import { fetchChartsData } from "@/utils/actions";
+import Chart from "./Chart";
+
+const ChartsContainer = async () => {
+  const bookings = await fetchChartsData();
+  if (bookings.length < 1) return null;
+
+  return <Chart data={bookings} />;
+
   return <div>ChartsContainer</div>;
 };
 
--- utils/format.ts
+++ utils/format.ts
@@ -12,10 +12,15 @@ export function formatQuantity(quantity: number, noun: string): string {
   return quantity === 1 ? `${quantity} ${noun}` : `${quantity} ${noun}s`;
 }
 
-export const formatDate = (date: Date) => {
-  return new Intl.DateTimeFormat("en-IN", {
+export const formatDate = (date: Date, onlyMonth?: boolean) => {
+  const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
     month: "long",
-    day: "numeric",
-  }).format(date);
+  };
+
+  if (!onlyMonth) {
+    options.day = "numeric";
+  }
+
+  return new Intl.DateTimeFormat("en-IN", options).format(date);
 };
